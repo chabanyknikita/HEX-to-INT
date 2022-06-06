8d567c74b902961f5b1fa0270d3b9690053ffa2c
Nikita Chabaniuk <92546152+chabanyknikita@users.noreply.github.com>
2022-06-06 14:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-hex_s = input("Write your value: ")
-byte = int(input("Write byte of your value = "))
-a = int(hex_s, 16)
 msgHB = "HEX to BigEndian:"
 msgBH = "BigEndian to HEX:"
 msgHL = "HEX to LittleEndian:"
 msgLH = "LittleEndian to HEX:"
 
+hex_s = input("Write your value: ")
+byte = int(len(str(hex_s)) / 2)
+a = int(hex_s, 16)
+
 
 def hex_to_BigEndian(hex_s):
-    print(f"\n{msgHB} \n{hex_s} -> {a}")
+    print(f"{msgHB} \n{hex_s} -> {a}")
 
 
 def BigEndian_to_hex(number):
@@ -19,6 +20,8 @@ def BigEndian_to_LittleEndian(value: int) -> int:
     return int.from_bytes(value.to_bytes(byte, byteorder="little"), byteorder="big")
 
 
+print(f"\nValue: {hex_s}")
+print(f"Number of bytes: {byte}")
 hex_to_BigEndian(hex_s)
 BigEndian_to_hex(a)
 print(f"{msgHL}\n{hex_s} -> {BigEndian_to_LittleEndian(a)}")
